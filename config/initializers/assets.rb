2ab004e6db4c276c0f9a5ea7f36b86c9018b4b30
Hug0xynum <hugo@oxynum.fr>
2015-08-10 20:16:24 +0200

    Remove changing url in case of error in signing up

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( application.css )
 	#Rails.application.config.assets.precompile += %w( users.css )
 	#Rails.application.config.assets.precompile += %w( pages.css )
 # Add additional assets to the asset load path
